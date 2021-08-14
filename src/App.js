6f72a4a77fa3931c1ffac59e8655908b3b1cb2a1
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-14 17:48:38 +0530

    created new react app removed unnecessary files for the tutorial modified App component to test basic component structure dynamic values in JSX syntax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const title = "Welcome to the new React blog";
+  const likes = 80;
+  const person = { name: "Isuru", age: 33 };
+  const link = "https://github.com/isu3ru/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Name is {person.name} and I am {person.age} years old.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>An Array: {[1, 2, 3]}</p>
+        <p>
+          A Math.random (will change on page refresh): {Math.random() * 100}
+        </p>
+        <p>
+          <a href={link} target="_blank" rel="noreferrer">
+            My Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
