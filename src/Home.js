c83d82dcab1275a59f3cc04bece1765133596a21
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-14 19:04:34 +0530

    event bindings and arguments passing arguments to event handler functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello Isuru", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("Mario", e)}>
+        Click Me Again
+      </button>
     </div>
   );
 };
