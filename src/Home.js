a50113cf0e680a4ad115411b7584c7f714f42e54
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-15 00:57:54 +0530

    added json-server added sample data for the json-server fetch blog posts from json-server and set in BlogList component removed delete handler code

--- src/Home.js
+++ src/Home.js
@@ -3,65 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   // set the list of blog posts to blogs property
-  const [blogPosts, setBlogPosts] = useState([
-    {
-      title: "First Post",
-      body: "lorem ipsum dolor",
-      author: "Isuru",
-      id: 1,
-    },
-    {
-      title: "Second Post",
-      body: "Amet sit dolor lorem",
-      author: "Chamikara",
-      id: 2,
-    },
-    {
-      title: "Third Post",
-      body: "ipsum dolor lorem sit amet",
-      author: "Isuru",
-      id: 3,
-    },
-  ]);
+  const [blogPosts, setBlogPosts] = useState(null);
 
-  const [name, setName] = useState("Isuru");
-
-  const handleDelete = (id) => {
-    // filter through the blog posts entries and return the entries without the given id
-    const filteredBlogPosts = blogPosts.filter(
-      (blogPost) => blogPost.id !== id
-    );
-    // in a real world application, we can remove the item from db too, at this moment.
-    // set the filtered list as the blogPosts
-    setBlogPosts(filteredBlogPosts);
+  const jsonServerEndpoints = {
+    allBlogs: "http://localhost:8000/blogs",
   };
 
   useEffect(() => {
-    console.log("use effect triggered");
-  }, [name]);
+    fetch(jsonServerEndpoints.allBlogs)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogPosts(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList
-        blogPosts={blogPosts}
-        title="All Blog Posts"
-        // passing the handleDelete function as a prop
-        handleDelete={handleDelete}
-      />
-      <BlogList
-        // get only the list of posts authored by Isuru
-        blogPosts={blogPosts.filter((post) => post.author === "Isuru")}
-        title="Isuru's Blog Posts"
-        handleDelete={handleDelete}
-      />
-      <BlogList
-        // get only the list of posts authored by Chamikara
-        blogPosts={blogPosts.filter((post) => post.author === "Chamikara")}
-        title="Chamikara's Blog Posts"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName("Isuru Ranawaka")}>Change Name</button>
-      <p>{name}</p>
+      {/* output the BlogList component only after the blogPosts value
+       is populated with the data fetched from the server */}
+      {blogPosts && <BlogList blogPosts={blogPosts} title="All Blog Posts" />}
     </div>
   );
 };
