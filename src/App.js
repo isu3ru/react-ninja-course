5a11a83106c1f2e3605b560c5e677bb756b367f2
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-14 18:36:16 +0530

    removed app.css file added new styles to index.css added an inline-style block in navbar Added a Home, Navbar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  return <div className="App">App Component</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
