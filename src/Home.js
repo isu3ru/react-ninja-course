f4bc4136351d809812d3e70e862f93daff884e53
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-16 01:59:05 +0530

    created custom hook useFetch to fetch data from an API endpoint use the custom hook useFetch to display data from the json-server
    removed unnecessary hook imports

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,13 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  // set the list of blog posts to blogs property
-  const [blogPosts, setBlogPosts] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  const jsonServerEndpoints = {
-    allBlogs: "http://localhost:8000/blogs",
-  };
-
-  useEffect(() => {
-    fetch(jsonServerEndpoints.allBlogs)
-      .then((res) => {
-        // you can check for HTTP status codes like 200, 404, etc... here
-        if (!res.status === 200) {
-          throw Error("Could not fetch the data for that resource.");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogPosts(data);
-        // set the isPending state to false so the loading message will be hidden
-        setIsPending(false);
-        // remove error message
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-      });
-  }, []);
+  // use the useFetch hook by sending in the url
+  const {
+    data: blogs, // allows to use the data property 'AS' 'blogs'
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs"); // the url as a parameter for the hook function
 
   return (
     <div className="home">
@@ -40,7 +16,7 @@ const Home = () => {
       {isPending && <div>Loading...</div>}
       {/* output the BlogList component only after the blogPosts value
        is populated with the data fetched from the server. */}
-      {blogPosts && <BlogList blogPosts={blogPosts} title="All Blog Posts" />}
+      {blogs && <BlogList blogPosts={blogs} title="All Blog Posts" />}
     </div>
   );
 };
---
+++ src/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+/**
+ * A custom hook for fetching data from a url
+ * allows to get the data from an endpoint
+ */
+const useFetch = (url) => {
+  // receive the url endpoint as a parameter
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        // you can check for HTTP status codes like 200, 404, etc... here
+        if (!res.status === 200) {
+          throw Error("Could not fetch the data for that resource.");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        // set the isPending state to false so the loading message will be hidden
+        setIsPending(false);
+        // remove error message
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, [url]);
+
+  return { data, isPending, error }; // return the props. this allows to use this hook like: 'const { data, isPending, error} = useFetch('http://localhost);' where this hook is used.
+};
+
+export default useFetch;
