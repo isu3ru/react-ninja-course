ea28f6918304059d548422af7c20b03fbb292a89
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-18 05:59:51 +0530

    updated project dependencies added router components added link components to navigation bar added new create component for create page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>DOJO Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog Post</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog Post</Link>
       </div>
     </nav>
   );
