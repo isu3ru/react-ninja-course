30c1b5a99286f8c3a9dca04a297aafecab6e67ca
Isuru Ranawaka <isuru@mybooking.lk>
2021-08-25 00:39:46 +0530

    created basic post form structure added styles for form components in create page added states and change handlers for the field values Removed display code lines for form input value states

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Isuru");
+
   return (
     <div className="create">
       <h2>Add New Blog Post</h2>
+      <form>
+        <div>
+          <label>Post Title</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Post Body</label>
+          <textarea
+            cols="30"
+            rows="10"
+            onChange={(e) => setBody(e.target.value)}
+          >
+            {body}
+          </textarea>
+        </div>
+        <div>
+          <label>Author</label>
+          <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+            <option value="Isuru">Isuru</option>
+            <option value="Chamikara">Chamikara</option>
+          </select>
+        </div>
+        <div>
+          <button>Add Blog</button>
+        </div>
+      </form>
     </div>
   );
 };
